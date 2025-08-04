8b461d0ba5d407ac14aa48cf151f01645f0fca1c
izar06 <izar.anam22@gmail.com>
2025-08-04 08:17:03 +0700

    Adjust Tugas 1

--- Tugas1.py
+++ Tugas1.py
@@ -10,10 +10,23 @@ contoh
 nama saya uno, saya tinggal di bogor. Penghasilan saya sebesar Rp. 100000000
 '''
 
-Nama = input("Masukkan nama anda: ")
-Kota = input("Masukkan kota tinggal anda: ")
-Gaji = input("Masukkan gaji anda: ")
-Pajak = 2.5 / 100
+#Opsi 1
+Nama = input ("Masukkan nama anda: ")
+Kota = input ("Masukkan kota tinggal anda: ")
+Gaji = input ("Masukkan gaji anda: ")
+Pajak = 2.5 / 100 # Karena tidak bisa input langsung 2.5%, jadinya saya ubah seperti ini
 gajiBersih = int(Gaji) * (1 - Pajak)
-# print(int(gajiBersih))
-print("Nama saya" + " " + Nama + "," + " " + "saya tinggal di" + " " + Kota + "." + " " + "Penghasilan saya sebesar Rp." + str(int(gajiBersih)))
+print(int(gajiBersih))
+print(type(gajiBersih))
+print("Nama saya " + Nama + "," + " " + "saya tinggal di " + Kota + "." + " " + "Penghasilan saya sebesar Rp. " + str(int(gajiBersih)))
+
+# Opsi 2
+nama = input ("Masukkan nama anda: ")
+kota = input ("Masukkan kota tinggal anda: ")
+gaji = input ("Masukkan gaji anda: ")
+perhitungan_gaji = int(gaji) * 0.025 # Menghitung gaji jika di kalikan 2.5%
+gaji_bersih = int(gaji) - int(perhitungan_gaji) # Lalu gaji di kurangin dengan hasil perhitungan gaji yang dikalikan 2.5%
+print(perhitungan_gaji)
+print(gaji_bersih)
+print(type(gaji_bersih))
+print("Nama saya " + nama + "," + " " + "saya tinggal di " + kota + "." + " " + "Penghasilan saya sebesar Rp. " + str(gaji_bersih))
